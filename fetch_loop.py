899b28b8528d3391f98a283dc0b5c63ee5547444
KennyZhou2022 <107938476+KennyZhou2022@users.noreply.github.com>
2025-07-08 22:09:49 +0800

    Add pollution fetcher

--- fetch_loop.py
+++ fetch_loop.py
@@ -1,11 +1,12 @@
 import time
-from fetch_weather import fetch_weather
+from fetch_weather import fetch_weather, fetch_pollution
 
 print("[*] Start fetching weather information")
 
 while True:
     try:
         fetch_weather()
+        fetch_pollution()
     except Exception as e:
         print(f"[!] Exception: {e}")
     time.sleep(600)  # Fetch data every 10 mins
--- fetch_weather.py
+++ fetch_weather.py
@@ -30,7 +30,7 @@ def fetch_weather():
 
     Path("/data/history").mkdir(parents=True, exist_ok=True)
     timestamp = datetime.utcnow().strftime("%Y%m%d%H%M%S")
-    history_path = f"/data/history/weather_{timestamp}.json"
+    history_path = f"/data/history_weather/weather_{timestamp}.json"
     latest_path = "/data/weather_latest.json"
 
     with open(history_path, "w", encoding="utf-8") as f:
@@ -40,3 +40,38 @@ def fetch_weather():
         f.write(response.text)
 
     print(f"✅ Weather data saved to {latest_path} and {history_path}")
+
+
+def fetch_pollution():
+    api_key = os.getenv("API_KEY")
+    http_proxy = os.getenv("HTTP_PROXY")
+    https_proxy = os.getenv("HTTPS_PROXY")
+
+    if not api_key:
+        raise ValueError("API_KEY is not set")
+
+    lat = "31.2222"
+    lon = "121.4581"
+
+    url = f"https://api.openweathermap.org/data/2.5/air_pollution/history?lat={lat}&lon={lon}&appid={api_key}"
+
+    proxies = {
+        "http": http_proxy,
+        "https": https_proxy
+    }
+
+    response = requests.get(url, proxies=proxies, timeout=20)
+    response.raise_for_status()
+
+    Path("/data/history").mkdir(parents=True, exist_ok=True)
+    timestamp = datetime.utcnow().strftime("%Y%m%d%H%M%S")
+    history_path = f"/data/history_pollution/pollution_{timestamp}.json"
+    latest_path = "/data/pollution_latest.json"
+
+    with open(history_path, "w", encoding="utf-8") as f:
+        f.write(response.text)
+
+    with open(latest_path, "w", encoding="utf-8") as f:
+        f.write(response.text)
+
+    print(f"✅ Pollution data saved to {latest_path} and {history_path}")
